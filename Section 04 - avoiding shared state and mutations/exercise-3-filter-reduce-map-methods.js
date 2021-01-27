c28242fa5f32e149c9b16c3238eda9fe071c3dd0
Igor <houwat@gmail.com>
2021-01-27 17:45:04 -0500

    section 3 - exercise 4: filter, map, reduce

--- Section 04 - avoiding shared state and mutations/exercise-3-filter-reduce-map-methods.js
+++ Section 04 - avoiding shared state and mutations/exercise-3-filter-reduce-map-methods.js
@@ -9,31 +9,23 @@ const multiplyByTen = scores.map(function(val) {
 
 //Remove any scores that are over 100.
 const removeOver100 = multiplyByTen.filter(function(val) {
-  return val < 100;
+  return val <= 100;
 });
-// removeOver100 --> [ 50, 60, 0, 10, 75, 80, 60, 90, 80, 0, 30 ] 
-
+// removeOver100 --> [ 50, 60, 100, 0, 10, 75, 80, 60, 90, 80, 0, 30 ] 
 //Remove any scores that are 0 or below.
 const removeZero = removeOver100.filter(function(val) {
   return val > 0;
 });
-// removeZero --> [ 50, 60, 10, 75, 80, 60, 90, 80, 30 ] 
+// removeZero --> [ 50, 60, 100, 10, 75, 80, 60, 90, 80, 30 ] 
 
 //Sum the scores.
 const sumOfScores = removeZero.reduce(function(acc, val) {
   return acc + val;
 }, 0);
-// sumOfScores --> 535
+// sumOfScores --> 635
 
 //Provide a count for the number of scores still remaining.
 const arrayLength = removeZero.reduce(function(accumulator) {
   return accumulator + 1;
 }, 0);
-// arrayLength --> 9
-
-
-
-
-
-
-
+// arrayLength --> 10
